6c14d2c4d27013b81d0b03a1785eb6101102634f
Kamila Suyargulova <kamila.suyargulova@yandex.ru>
2026-02-02 23:19:04 +0300

    ТЗ14

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+@Data
+@Builder
 public class Item {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    @NotBlank
+    private User owner;
+    private ItemRequest request;
 }
